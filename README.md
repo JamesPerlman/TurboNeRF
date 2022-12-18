d1f30807ca1080f0394516a80b6448ce8d2ec5ce
JamesPerlman <jam.e.perl@gmail.com>
2022-12-17 19:13:45 -0800

    Initial commit - working cmake project with Hello Cuda and tiny-cuda-nn

---
+++ README.md
@@ -0,0 +1,6 @@
+## THANK YOU
+
+Many thank-yous to open source projects that allowed this project to reach its full potential:
+
+https://github.com/pkestene/cuda-proj-tmpl [[LICENSE](https://github.com/pkestene/cuda-proj-tmpl/blob/master/LICENSE)]
+https://github.com/NVlabs/tiny-cuda-nn [[LICENSE](https://github.com/NVlabs/tiny-cuda-nn/blob/master/LICENSE.txt)]
---
+++ src/main.cu
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cuda_runtime.h>
+
+#include <tiny-cuda-nn/common.h>
+
+#include "main.h"
+
+// Declare a global device function
+__global__ void helloCuda(void)
+{
+    printf("Hello World from GPU!\n");
+}
+
+int main()
+{
+    // Launch the "helloCuda" kernel on the device
+    helloCuda<<<1, 1>>> ();
+
+    // Wait for the kernel to finish executing
+    cudaDeviceSynchronize();
+
+    return 0;
+}
