49bb058796bf5eac4dd9a54aafa4f7e7e38d3ccc
JamesPerlman <jam.e.perl@gmail.com>
2022-12-20 09:22:10 -0800

    add ray generator (pinhole camera model)

---
+++ src/models/camera.cu
@@ -0,0 +1,40 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include "camera.hpp"
+#include "ray.hpp"
+
+
+using namespace nrc;
+using namespace Eigen;
+
+// init_rays CUDA kernel
+NRC_HOST_DEVICE void init_rays_pinhole(
+	uint32_t n_rays,
+	Camera cam,
+	Ray* rays_out
+) {
+	int32_t x = blockIdx.x * blockDim.x + threadIdx.x;
+	int32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+	
+	int32_t idx = x * gridDim.x + y;
+	
+	if (idx >= n_rays) {
+		return;
+	}
+	// uv ranges [-0.5f, 0.5f] in both dimensions, and is centered on this pixel
+	auto uv = Vector2f(
+		float(x) / (float(cam.pixel_dims.x()) + 0.5f),
+		float(y) / (float(cam.pixel_dims.y()) + 0.5f)
+	) - Vector2f(0.5f);
+
+	auto pix_pos = Vector3f(uv * cam.sensor_size, 1.0f);
+	
+	auto cam_origin = cam.transform.block<3, 1>(0, 3);
+
+	auto ray_d = pix_pos - cam_origin;
+	auto ray_o = cam_origin + cam.near * ray_d;
+
+	Ray ray = { ray_o, ray_d };
+	rays_out[idx] = ray;
+}
