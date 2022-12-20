1d284a156e4ea19bd9132037e5d97d4b454b9fe7
JamesPerlman <jam.e.perl@gmail.com>
2022-12-20 09:21:32 -0800

    extra props in camera

--- src/models/camera.hpp
+++ src/models/camera.hpp
@@ -9,16 +9,18 @@ NRC_NAMESPACE_BEGIN
 struct Camera {
 	float near;
 	float far;
-	Eigen::Vector2i dimensions;
 	Eigen::Vector2f focal_length;
+	Eigen::Vector2i pixel_dims;
+	Eigen::Vector2f sensor_size;
 	Eigen::Matrix4f transform;
 
 	// constructor
-	Camera(float near, float far, Eigen::Vector2i dimensions, Eigen::Vector2f focal_length, Eigen::Matrix4f transform) {
+	Camera(float near, float far, Eigen::Vector2f focal_length, Eigen::Vector2i pixel_dims, Eigen::Vector2f sensor_size, Eigen::Matrix4f transform) {
 		this->near = near;
 		this->far = far;
-		this->dimensions = dimensions;
 		this->focal_length = focal_length;
+		this->pixel_dims = pixel_dims;
+		this->sensor_size = sensor_size;
 		this->transform = transform;
 	}
 };
