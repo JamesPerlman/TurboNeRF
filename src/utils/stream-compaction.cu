e74d398768b40571e1b10e8fb22c91a5456ca0ad
JamesPerlman <jam.e.perl@gmail.com>
2023-01-30 16:52:09 -0800

    add some more compaction helpers

--- src/utils/stream-compaction.cu
+++ src/utils/stream-compaction.cu
@@ -1,9 +1,5 @@
 #include "stream-compaction.cuh"
-
-#include <thrust/copy.h>
 #include <thrust/count.h>
-#include <thrust/device_ptr.h>
-#include <thrust/iterator/counting_iterator.h>
 
 NRC_NAMESPACE_BEGIN
 
@@ -38,6 +34,48 @@ void generate_compaction_indices(
         predicate_ptr,
         indices_ptr,
         thrust::detail::equal_to_value<const bool>(true)
+    );
+}
+
+template <typename T>
+struct nonzero {
+    __host__ __device__
+    bool operator()(const T& x) const {
+        return x != 0;
+    }
+};
+
+size_t count_nonzero_elements(
+    const cudaStream_t& stream,
+    const size_t n_elements,
+    const uint32_t* predicate
+) {
+    thrust::device_ptr<const uint32_t> predicate_ptr(predicate);
+    return thrust::count_if(
+        thrust::cuda::par.on(stream),
+        predicate_ptr,
+        predicate_ptr + n_elements,
+        nonzero<uint32_t>()
+    );
+};
+
+void generate_nonzero_compaction_indices(
+    const cudaStream_t& stream,
+    const size_t n_elements,
+    const uint32_t* values,
+    int* indices
+) {
+    thrust::device_ptr<const bool> values_ptr(values);
+    thrust::device_ptr<int> indices_ptr(indices);
+
+    thrust::counting_iterator<int> counting(0);
+    thrust::copy_if(
+        thrust::cuda::par.on(stream),
+        counting,
+        counting + n_elements,
+        values_ptr,
+        indices_ptr,
+        nonzero<uint32_t>()
     );
 }
 
--- src/utils/stream-compaction.cuh
+++ src/utils/stream-compaction.cuh
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <thrust/copy.h>
+#include <thrust/device_ptr.h>
+#include <thrust/iterator/counting_iterator.h>
+
 #include "../common.h"
 
 NRC_NAMESPACE_BEGIN
@@ -14,6 +18,19 @@ void generate_compaction_indices(
     const cudaStream_t& stream,
     const size_t n_elements,
     const bool* predicate,
+    int* indices
+);
+
+size_t count_nonzero_elements(
+    const cudaStream_t& stream,
+    const size_t n_elements,
+    const uint32_t* input
+);
+
+void generate_nonzero_compaction_indices(
+    const cudaStream_t& stream,
+    const size_t n_elements,
+    const uint32_t* values,
     int* indices
 );
 
