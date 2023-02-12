5f7eb4e038bdfc513c569571c101dbad957fef35
JamesPerlman <jam.e.perl@gmail.com>
2023-02-11 20:31:26 -0800

    fix OpenGL calls in BlenderRenderEngine class

--- src/integrations/blender.cuh
+++ src/integrations/blender.cuh
@@ -18,20 +18,31 @@ private:
 
     bool is_initialized = false;
 
-    GLfloat tex_coords[8] = {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        0.0f, 1.0f
-    };
+
 
     static BlenderRenderEngine& _getInstance()
     {
         static BlenderRenderEngine instance;
         return instance;
     }
+    
+    void _init()
+    {
+        if (!is_initialized)
+        {
+            if (glewInit() != GLEW_OK)
+                throw std::runtime_error("Failed to initialize GLEW.");
+
+            is_initialized = true;
+        }
+    }
 
 public:
+
+    static void init()
+    {
+        _getInstance()._init();
+    }
 
     /**
      * The following functionality was adapted from the Pixar RenderMan for Blender plugin.
@@ -39,33 +50,30 @@ public:
      * 
      */
 
-    static void draw(OpenGLRenderSurface& render_surface)
+    static void draw(OpenGLRenderSurface* render_surface)
     {
         // AFAIK this is completely undocumented by Blender.
         // It allows us to draw into a Blender viewport, keeping all data on the GPU.
 
         // These are the vertices of a quad that covers the entire viewport.
-        const float w = static_cast<float>(render_surface.width);
-        const float h = static_cast<float>(render_surface.height);
+        const float w = static_cast<float>(render_surface->width);
+        const float h = static_cast<float>(render_surface->height);
 
-        float vertices[8] = {
+        GLfloat vertices[8] = {
             0.0f, 0.0f,
                w, 0.0f,
                w,    h,
             0.0f,    h
         };
 
-        glTexImage2D(
-            GL_TEXTURE_2D,
-            0,
-            GL_RGBA,
-            render_surface.width,
-            render_surface.height,
-            0,
-            GL_RGBA,
-            GL_FLOAT,
-            NULL
-        );
+        GLfloat tex_coords[8] = {
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f,
+            0.0f, 1.0f
+        };
+        
+        //glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, render_surface->width, render_surface->height, GL_RGBA, GL_FLOAT, NULL);
 
         GLint shader_program_id;
         glGetIntegerv(GL_CURRENT_PROGRAM, &shader_program_id);
@@ -85,38 +93,31 @@ public:
         GLuint texture_vbo_id;
         glGenBuffers(1, &texture_vbo_id);
         glBindBuffer(GL_ARRAY_BUFFER, texture_vbo_id);
-        glVertexAttribPointer(
-            texcoord_location, // vertex attrib location in shader
-            2, // num components per vertex
-            GL_FLOAT, // data type
-            GL_FALSE, // normalized? (no)
-            2 * sizeof(GLfloat), // stride
-            reinterpret_cast<void*>(0) // offset
-        );
-        
+        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), &tex_coords[0], GL_STATIC_DRAW);
+        glVertexAttribPointer(texcoord_location, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), reinterpret_cast<void*>(0));
+
         GLuint vertex_vbo_id;
         // Generate a vertex buffer object for the positions
         glGenBuffers(1, &vertex_vbo_id);
         glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo_id);
-        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), &vertices[0], GL_STATIC_DRAW);
-        
+        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+
         // Enable the "pos" attribute
         glEnableVertexAttribArray(position_location);
-        // Specify the format and organization of the position attribute in the buffer object
-        glVertexAttribPointer(position_location, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), reinterpret_cast<void*>(0));
+        glVertexAttribPointer(position_location, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), reinterpret_cast<void*>(0));
 
         glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the current buffer
         glBindVertexArray(0); // Unbind the vertex array object
 
         // Activate texture unit 0
         glActiveTexture(GL_TEXTURE0);
+
         // Bind to render surface texture
-        glBindTexture(GL_TEXTURE_2D, render_surface.get_texture_id());
+        glBindTexture(GL_TEXTURE_2D, render_surface->get_texture_id());
         glBindVertexArray(vertex_array);
 
         // Draw the triangle fan
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-        
         glBindVertexArray(0); // Unbind the vertex array object
         glDeleteVertexArrays(1, &vertex_array); // Delete the vertex array object
         glBindTexture(GL_TEXTURE_2D, 0); // Unbind the texture
