16ab74316016381f7e8e35e7f3808f3848909fd2
JamesPerlman <jam.e.perl@gmail.com>
2023-02-11 16:03:31 -0800

    add render callbacks to render request

--- src/models/render-request.cuh
+++ src/models/render-request.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <stdint.h>
 
 #include "../render-targets/render-target.cuh"
@@ -13,9 +14,22 @@ struct RenderRequest {
     const Camera camera;
     std::vector<NeRFProxy*> proxies;
     RenderTarget* output;
+    std::function<void(bool)> on_result;
+    std::function<bool()> should_cancel;
 
-    RenderRequest(const Camera& camera, std::vector<NeRFProxy*>& proxies, RenderTarget* output)
-        : camera(camera), proxies(proxies), output(output) {};
+    RenderRequest(
+        const Camera& camera,
+        std::vector<NeRFProxy*>& proxies,
+        RenderTarget* output,
+        std::function<void(bool)> on_result = nullptr,
+        std::function<bool()> should_cancel = nullptr
+    )
+        : camera(camera)
+        , proxies(proxies)
+        , output(output)
+        , on_result(on_result)
+        , should_cancel(should_cancel)
+    { };
 };
 
 NRC_NAMESPACE_END
