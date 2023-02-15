50f919a9268f2df78df3e3db759da763e383f0c4
JamesPerlman <jam.e.perl@gmail.com>
2023-02-15 04:22:35 -0800

    rework network params workspace

--- src/workspaces/network-params-workspace.cuh
+++ src/workspaces/network-params-workspace.cuh
@@ -11,6 +11,10 @@ struct NetworkParamsWorkspace: Workspace {
 
     using Workspace::Workspace;
 
+    float* params_fp;
+    tcnn::network_precision_t* params_hp;
+    tcnn::network_precision_t* gradients_hp;
+
     float* density_network_params_fp;
     tcnn::network_precision_t* density_network_params_hp;
     tcnn::network_precision_t* density_network_gradients_hp;
@@ -26,13 +30,19 @@ struct NetworkParamsWorkspace: Workspace {
     ) {
         free_allocations();
 
-        density_network_params_fp = allocate<float>(stream, n_density_network_params);
-        density_network_params_hp = allocate<tcnn::network_precision_t>(stream, n_density_network_params);
-        density_network_gradients_hp = allocate<tcnn::network_precision_t>(stream, n_density_network_params);
+        const uint32_t n_params = n_density_network_params + n_color_network_params;
 
-        color_network_params_fp = allocate<float>(stream, n_color_network_params);
-        color_network_params_hp = allocate<tcnn::network_precision_t>(stream, n_color_network_params);
-        color_network_gradients_hp = allocate<tcnn::network_precision_t>(stream, n_color_network_params);
+        params_fp = allocate<float>(stream, n_params);
+        params_hp = allocate<tcnn::network_precision_t>(stream, n_params);
+        gradients_hp = allocate<tcnn::network_precision_t>(stream, n_params);
+
+        density_network_params_fp = params_fp;
+        density_network_params_hp = params_hp;
+        density_network_gradients_hp = gradients_hp;
+
+        color_network_params_fp = params_fp + n_density_network_params;
+        color_network_params_hp = params_hp + n_density_network_params;
+        color_network_gradients_hp = gradients_hp + n_density_network_params;
     }
 };
 
