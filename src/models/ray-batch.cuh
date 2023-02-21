139efee260ad9db87fc49c0d2293607638496923
JamesPerlman <jam.e.perl@gmail.com>
2023-02-20 16:49:35 -0800

    add RayBatch

---
+++ src/models/ray-batch.cuh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../common.h"
+
+NRC_NAMESPACE_BEGIN
+
+struct RayBatch {
+    int size; // number of rays in this batch
+    int stride; // stride between xyz components
+    float* pos;
+    float* dir;
+    float* idir;
+    float* t;
+    float* transmittance;
+    int* index;
+    uint8_t* flags; // optimization to avoid multiple global memory accesses
+};
+
+enum class RayFlags: uint8_t {
+    Alive = 0,
+    Active = 1
+};
+
+NRC_NAMESPACE_END
