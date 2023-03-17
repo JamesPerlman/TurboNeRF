2c00912af8b0f51f91f49d2e968efb2fac4e741a
JamesPerlman <jam.e.perl@gmail.com>
2023-03-17 01:11:22 -0700

    consts back to larger step size and occ thresh

--- src/utils/nerf-constants.cuh
+++ src/utils/nerf-constants.cuh
@@ -4,7 +4,7 @@
 
 namespace NeRFConstants {
     // min_step_size = sqrt(3.0f) / 1024.0f;
-    constexpr float min_step_size = 0.00169145586f;
+    constexpr float min_step_size = 0.007f;//0.00169145586f;
 
     constexpr float cone_angle = 1.0f / 256.0f;
 
@@ -17,7 +17,7 @@ namespace NeRFConstants {
 	// This is adapted from the instant-NGP paper.  See page 15 on "Updating occupancy grids"
 	// For some reason, the way the paper says it does not work for this implementation.
 	// It seems to work with a threshold of 0.01, when the paper says to multiply by min_step_size.
-    constexpr float occupancy_threshold = 0.01f * NeRFConstants::min_step_size;
+    constexpr float occupancy_threshold = 0.01f;// * NeRFConstants::min_step_size;
 
     constexpr uint32_t n_steps_per_render_compaction = 64;
 }
