870ea4d4702386b6cfb2e3fd19361017869a7f80
JamesPerlman <jam.e.perl@gmail.com>
2023-03-17 01:11:38 -0700

    split camera kernels into header/impl files

---
+++ src/utils/camera-kernels.cu
@@ -0,0 +1,46 @@
+#include "../utils/device-math.cuh"
+#include "camera-kernels.cuh"
+
+TURBO_NAMESPACE_BEGIN
+
+__global__ void generate_undistorted_pixel_map_kernel(
+    const uint32_t n_pixels,
+    const Camera camera,
+    float* __restrict__ out_buf
+) {
+    const uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx >= n_pixels) {
+        return;
+    }
+    
+    const uint32_t& w = camera.resolution.x;
+
+    const float& k1 = camera.dist_params.k1;
+    const float& k2 = camera.dist_params.k2;
+    const float& k3 = camera.dist_params.k3;
+
+    const float& p1 = camera.dist_params.p1;
+    const float& p2 = camera.dist_params.p2;
+
+    const int y = divide(idx, w);
+    const int x = idx - y * w;
+
+    const float xd = (float(x) - camera.principal_point.x) / camera.focal_length.x;
+    const float yd = (float(y) - camera.principal_point.y) / camera.focal_length.y;
+
+    float xu, yu;
+    radial_and_tangential_undistort(
+        xd, yd,
+        k1, k2, k3,
+        p1, p2,
+        1e-9f,
+        10,
+        xu, yu
+    );
+
+    out_buf[idx + 0 * n_pixels] = xu * camera.focal_length.x + camera.principal_point.x;
+    out_buf[idx + 1 * n_pixels] = yu * camera.focal_length.y + camera.principal_point.y;
+}
+
+TURBO_NAMESPACE_END
--- src/utils/camera-kernels.cuh
+++ src/utils/camera-kernels.cuh
@@ -3,7 +3,6 @@
 #include "../common.h"
 
 #include "../models/camera.cuh"
-#include "../utils/device-math.cuh"
 
 TURBO_NAMESPACE_BEGIN
 
@@ -14,7 +13,7 @@ inline __device__ void compute_residual_and_jacobian(
     // inputs
     const float& x, const float& y,
     const float& xd, const float& yd,
-    const float& k1, const float& k2, const float& k3, const float& k4,
+    const float& k1, const float& k2, const float& k3,
     const float& p1, const float& p2,
 
     // outputs
@@ -23,9 +22,9 @@ inline __device__ void compute_residual_and_jacobian(
     float& fy_x, float& fy_y
 ) {
     // let r(x, y) = x^2 + y^2;
-    //     d(x, y) = 1 + k1 * r(x, y) + k2 * r(x, y) ^2 + k3 * r(x, y)^3 + k4 * r(x, y)^4;
+    //     d(x, y) = 1 + k1 * r(x, y) + k2 * r(x, y) ^2 + k3 * r(x, y)^3;
     const float r = x * x + y * y;
-    const float d = 1.0 + r * (k1 + r * (k2 + r * (k3  + r * k4)));
+    const float d = 1.0 + r * (k1 + r * (k2 + r * k3));
 
     // The perfect projection is:
     // xd = x * d(x, y) + 2 * p1 * x * y + p2 * (r(x, y) + 2 * x^2);
@@ -42,7 +41,7 @@ inline __device__ void compute_residual_and_jacobian(
     fy = d * y + 2 * p2 * x * y + p1 * (r + 2 * y * y) - yd;
 
     // Compute derivative of d over [x, y]
-    const float d_r = (k1 + r * (2.0 * k2 + r * (3.0 * k3 + r * 4.0 * k4)));
+    const float d_r = k1 + r * (2.0 * k2 + r * (3.0 * k3));
     const float d_x = 2.0 * x * d_r;
     const float d_y = 2.0 * y * d_r;
 
@@ -58,7 +57,7 @@ inline __device__ void compute_residual_and_jacobian(
 // Copilot generated this.  Modified to match nerfies:  https://github.com/google-research/multinerf/blob/main/internal/camera_utils.py#L477
 inline __device__ void radial_and_tangential_undistort(
     const float& xd, const float& yd,
-    const float& k1, const float& k2, const float& k3, const float& k4,
+    const float& k1, const float& k2, const float& k3,
     const float& p1, const float& p2,
     const float& eps,
     const int& max_iterations,
@@ -75,7 +74,7 @@ inline __device__ void radial_and_tangential_undistort(
         compute_residual_and_jacobian(
             x, y,
             xd, yd,
-            k1, k2, k3, k4,
+            k1, k2, k3,
             p1, p2,
             fx, fy,
             fx_x, fx_y, fy_x, fy_y
@@ -106,41 +105,6 @@ __global__ void generate_undistorted_pixel_map_kernel(
     const uint32_t n_pixels,
     const Camera camera,
     float* __restrict__ out_buf
-) {
-    const uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (idx >= n_pixels) {
-        return;
-    }
-    
-    const uint32_t& w = camera.resolution.x;
-
-    const float& k1 = camera.dist_params.k1;
-    const float& k2 = camera.dist_params.k2;
-    const float& k3 = camera.dist_params.k3;
-    const float& k4 = camera.dist_params.k4;
-
-    const float& p1 = camera.dist_params.p1;
-    const float& p2 = camera.dist_params.p2;
-
-    const int y = divide(idx, w);
-    const int x = idx - y * w;
-
-    const float xd = (float(x) - camera.principal_point.x) / camera.focal_length.x;
-    const float yd = (float(y) - camera.principal_point.y) / camera.focal_length.y;
-
-    float xu, yu;
-    radial_and_tangential_undistort(
-        xd, yd,
-        k1, k2, k3, k4,
-        p1, p2,
-        1e-9f,
-        10,
-        xu, yu
-    );
-
-    out_buf[idx + 0 * n_pixels] = xu * camera.focal_length.x + camera.principal_point.x;
-    out_buf[idx + 1 * n_pixels] = yu * camera.focal_length.y + camera.principal_point.y;
-}
+);
 
 TURBO_NAMESPACE_END
