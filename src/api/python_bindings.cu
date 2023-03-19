d1153ed83be2d337f15a8cb2f5d68ce354698fb4
JamesPerlman <jam.e.perl@gmail.com>
2023-03-18 18:57:12 -0700

    version microbump

--- src/api/python_bindings.cu
+++ src/api/python_bindings.cu
@@ -41,7 +41,7 @@ PYBIND11_MODULE(PyTurboNeRF, m) {
      */
 
     m.doc() = "TurboNeRF Python Bindings";
-    m.attr("__version__") = "0.0.3";
+    m.attr("__version__") = "0.0.4-1";
 
     /**
      * Global functions
