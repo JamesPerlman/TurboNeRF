0f7d96e40a9a762125156c4e9f221d13be916062
JamesPerlman <jam.e.perl@gmail.com>
2023-04-26 14:07:37 -0700

    move copy_gradients_kernel to common kernels

--- src/utils/common-network-kernels.cuh
+++ src/utils/common-network-kernels.cuh
@@ -38,4 +38,26 @@ __global__ void sigma_to_alpha_forward_kernel(
 	alpha[i] = sigma_to_alpha(sigma[i], dt[i]);
 }
 
+template <int N_CHANNELS, bool ACCUMULATE>
+__global__ void copy_gradients_kernel(
+	const uint32_t n_elements,
+	const uint32_t data_stride,
+	const float scale,
+	const float* __restrict__ input,
+	tcnn::network_precision_t* __restrict__ output
+) {
+	uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (idx >= n_elements) return;
+
+	#pragma unroll
+	for (int j = 0; j < N_CHANNELS; ++j) {
+		if (ACCUMULATE)
+			output[idx] = (tcnn::network_precision_t)(scale * input[idx]) + output[idx];
+		else
+			output[idx] = (tcnn::network_precision_t)(scale * input[idx]);
+		
+		idx += data_stride;
+	}
+}
 
--- src/utils/training-network-kernels.cuh
+++ src/utils/training-network-kernels.cuh
@@ -10,29 +10,6 @@
 
 TURBO_NAMESPACE_BEGIN
 
-template <int N_CHANNELS, bool ACCUMULATE>
-__global__ void copy_gradients_kernel(
-	const uint32_t n_elements,
-	const uint32_t data_stride,
-	const float scale,
-	const float* __restrict__ input,
-	tcnn::network_precision_t* __restrict__ output
-) {
-	uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (idx >= n_elements) return;
-
-	#pragma unroll
-	for (int j = 0; j < N_CHANNELS; ++j) {
-		if (ACCUMULATE)
-			output[idx] = (tcnn::network_precision_t)(scale * input[idx]) + output[idx];
-		else
-			output[idx] = (tcnn::network_precision_t)(scale * input[idx]);
-		
-		idx += data_stride;
-	}
-}
-
 /**
  * Apply exponential scaling to density network output
  * (log-space density!)
