35cd9aaa5a2ec7eade4f4ca05700cbe02f3301c8
JamesPerlman <jam.e.perl@gmail.com>
2023-04-26 22:14:51 -0700

    remove unnecessary allocations (saves ~176MB VRAM)

--- src/workspaces/network-workspace.cuh
+++ src/workspaces/network-workspace.cuh
@@ -14,20 +14,8 @@ struct NetworkWorkspace : Workspace {
     uint32_t batch_size;
 
     // gradient calculation buffers
-    float* sigma_buf; // e^(sigma - 1) 
-    float* alpha_buf;
-    float* trans_buf;
-    float* weight_buf; // alpha * transmittance
-    float* pxdiff_buf; // pixel channel differences
-    float* ray_rgba; // accumulated ray colors from samples
     float* loss_buf;
-    float* grad_dL_dR;
-    float* grad_dL_dweight;
-    float* grad_dL_dsigma;
-    float* grad_dL_dcolor;
     float* grad_dL_ddensity;
-    float* grad_dL_dalpha;
-    float* grad_dL_dtrans;
 
     float* density_network_dL_dinput;
     tcnn::network_precision_t* density_network_dL_doutput;
@@ -51,21 +39,8 @@ struct NetworkWorkspace : Workspace {
         density_network_dL_dinput = allocate<float>(stream, density_network_input_width * batch_size);
         color_network_dL_dinput = allocate<tcnn::network_precision_t>(stream, color_network_input_width * batch_size);
 
-        sigma_buf = allocate<float>(stream, batch_size);
-        alpha_buf = allocate<float>(stream, batch_size);
-        trans_buf = allocate<float>(stream, batch_size);
-        weight_buf = allocate<float>(stream, batch_size);
-        pxdiff_buf = allocate<float>(stream, 4 * batch_size);
-        ray_rgba = allocate<float>(stream, 4 * batch_size);
         loss_buf = allocate<float>(stream, 4 * batch_size);
-       
-        grad_dL_dR = allocate<float>(stream, 3 * batch_size);
-        grad_dL_dweight = allocate<float>(stream, batch_size);
-        grad_dL_dsigma = allocate<float>(stream, batch_size);
-        grad_dL_dcolor = allocate<float>(stream, 3 * batch_size);
         grad_dL_ddensity = allocate<float>(stream, batch_size);
-        grad_dL_dalpha = allocate<float>(stream, batch_size);
-        grad_dL_dtrans = allocate<float>(stream, batch_size);
 
         density_network_dL_doutput = allocate<tcnn::network_precision_t>(stream, density_network_output_width * batch_size);
         color_network_dL_doutput = allocate<tcnn::network_precision_t>(stream, color_network_output_width * batch_size);
